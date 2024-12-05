7f2eba3ec973c6c8619c34c0f9b005e7cfc4c9cb
SULINKOFICIAL <128751721+SULINKOFICIAL@users.noreply.github.com>
2024-12-04 23:22:21 -0300

    Update

--- resources/views/pages/clients/_form.blade.php
+++ resources/views/pages/clients/_form.blade.php
@@ -8,8 +8,8 @@
         <input type="text" class="form-control form-control-solid" placeholder="www.dominio.com.br" name="domain" value="{{ $content->domain ?? old('domain') }}" maxlength="255" required>
     </div>
     <div class="col-12 col-md-6 mb-4">
-        <label class="form-label fs-6 fw-bold text-gray-700 mb-2 required">Token</label>
-        <input type="password" class="form-control form-control-solid" placeholder="*******" name="token" value="{{ $content->token ?? old('token') }}" maxlength="255" required>
+        <label class="form-label fs-6 fw-bold text-gray-700 mb-2">Token</label>
+        <input type="password" class="form-control form-control-solid" placeholder="*******" name="token" value="" maxlength="255">
     </div>
     <div class="col-12 col-md-6 mb-4">
         <label class="form-label fs-6 fw-bold text-gray-700 mb-2">Logo</label>
