83fa439ce93ec6270472c15c384354fcfc312594
JeandreoFurquim <jeandreofur@gmail.com>
2025-02-20 11:32:57 -0300

    Update

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Http\Controllers\ApisController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 
 Route::get('/get-database', [ApisController::class, 'getDatabase']);
 Route::post('/error', [ApisController::class, 'notifyErrors']);
