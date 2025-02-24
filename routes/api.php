d907c3b1e3acf5fc86b62442e21a513f02505da2
SULINKOFICIAL <128751721+SULINKOFICIAL@users.noreply.github.com>
2025-02-23 21:49:51 -0300

    Update

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,18 @@
 use App\Http\Controllers\ApisController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/get-database', [ApisController::class, 'getDatabase']);
-Route::post('/error', [ApisController::class, 'notifyErrors']);
-Route::post('/tickets', [ApisController::class, 'tickets']);
+/**
+ * Api para comunicação com o WebSite micore.com.br
+ */
+Route::prefix('micore')->group(function () {
+    Route::get('/cadastrar-se', [ApisController::class, 'getDatabase']);
+});
+
+/**
+ * Api para comunicação entre os sistemas core com a central
+ */
+Route::prefix('central')->group(function () {
+    Route::get('/get-database', [ApisController::class, 'getDatabase']);
+    Route::post('/error', [ApisController::class, 'notifyErrors']);
+    Route::post('/tickets', [ApisController::class, 'tickets']);
+});
