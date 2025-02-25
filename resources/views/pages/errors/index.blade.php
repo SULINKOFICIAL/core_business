73ecf7bd5094ca0e6c280a4aef9a828d34073145
IsPrt <isispertsbr@gmail.com>
2025-02-25 18:17:26 -0300

    update

--- resources/views/pages/errors/index.blade.php
+++ resources/views/pages/errors/index.blade.php
@@ -7,24 +7,10 @@
     Erros do Sistema
 </p>
 <div class="card">
-    <div class="card-body">
-        <table class="table table-striped table-row-bordered gy-2 gs-7 align-middle datatables">
-            <thead class="rounded" style="background: #1c283e">
-                <tr class="fw-bold fs-6 text-white px-7">
-                    <th class="text-start" width="3%">Cliente</th>
-                    <th class="text-start">Mensagem</th>
-                    <th class="text-start px-0" width="30%">Código</th>
-                    <th class="text-center px-0">Código de Status</th>
-                    <th class="text-center px-0">Endereço Ip</th>
-                    <th class="text-center px-0">Criado Em</th>
-                </tr>
-            </thead>
-            <tbody id="errors-table">
-                {{-- RESULTS HERE --}}
-                {{-- RESULTS HERE --}}
-                {{-- RESULTS HERE --}}
-            </tbody>
-        </table>
+    <div class="card-body" id="errors-table">
+        {{-- RESULTS HERE --}}
+        {{-- RESULTS HERE --}}
+        {{-- RESULTS HERE --}}
     </div>
 </div>
 @endsection
@@ -46,6 +32,7 @@
             url: url,
             success: function(response){
                 $('#errors-table').html(response);
+                loadTables();
             }
         });
     })
--- resources/views/pages/errors/show.blade.php
+++ resources/views/pages/errors/show.blade.php
@@ -1,12 +1,26 @@
-@foreach ($contents as $content)
-    <tr>
-        <td class="text-center pe-8">{{ $content->client_id }}</td>
-        <td>
-            <a class="text-gray-800 text-hover-primary" href="{{ $content->url }}" target="_blank" data-bs-toggle="tooltip" title="{{ $content->message }}">{{ Str::limit($content->message, 50) }}</a>
-        </td>
-        <td class="text-start cursor-pointer text-hover-primary" data-bs-toggle="modal" data-bs-target="#modal-code" data-stacktrace="{{ $content->stack_trace }}" >{{ Str::limit($content->stack_trace, 50) }}</td>
-        <td class="text-center ">{{ $content->status_code }}</td>
-        <td class="text-center">{{ $content->ip_address }}</td>
-        <td class="text-center">{{ $content->created_at->format('d/m/Y')}}</td>
-    </tr>
-@endforeach
+<table id="dataTables" class="table table-striped table-row-bordered gy-2 gs-7 align-middle datatables">
+    <thead class="rounded" style="background: #1c283e">
+        <tr class="fw-bold fs-6 text-white px-7">
+            <th class="text-start" width="3%">Cliente</th>
+            <th class="text-start">Mensagem</th>
+            <th class="text-start px-0" width="30%">Código</th>
+            <th class="text-center px-0">Código de Status</th>
+            <th class="text-center px-0">Endereço Ip</th>
+            <th class="text-center px-0">Criado Em</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($contents as $content)
+            <tr>
+                <td class="text-center pe-8">{{ $content->client_id }}</td>
+                <td>
+                    <a class="text-gray-800 text-hover-primary" href="{{ $content->url }}" target="_blank" data-bs-toggle="tooltip" title="{{ $content->message }}">{{ Str::limit($content->message, 50) }}</a>
+                </td>
+                <td class="text-start cursor-pointer text-hover-primary" data-bs-toggle="modal" data-bs-target="#modal-code" data-stacktrace="{{ $content->stack_trace }}" >{{ Str::limit($content->stack_trace, 50) }}</td>
+                <td class="text-center ">{{ $content->status_code }}</td>
+                <td class="text-center">{{ $content->ip_address }}</td>
+                <td class="text-center">{{ $content->created_at->format('d/m/Y')}}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
