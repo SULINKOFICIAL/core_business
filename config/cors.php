b61b306693bf663be14b9c3efd00e6042bf2f487
JeandreoFurquim <jeandreofur@gmail.com>
2025-02-25 12:05:50 -0300

    Update

--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,34 @@
 <?php
 
 return [
-    'paths' => ['api/*'],                           // Selecione as rotas da API para aplicar o CORS
-    'allowed_methods' => ['*'],                     // Permitir todos os métodos HTTP
-    'allowed_origins' => ['https://micore.com.br'], // Defina o domínio de origem permitido
-    'allowed_headers' => ['*'],                     // Permitir todos os cabeçalhos
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['https://micore.com.br'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
+
     'supports_credentials' => false,
+
 ];
