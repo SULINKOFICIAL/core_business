36fd94848cf7986340ee913ad03aaf190b1ed0ef
JeandreoFurquim <jeandreofur@gmail.com>
2025-02-27 16:31:43 -0300

    Update

--- resources/views/pages/sectors/index.blade.php
+++ resources/views/pages/sectors/index.blade.php
@@ -10,38 +10,28 @@
     @foreach ($sectors as $sector)
     <div class="col-3 d-flex">
         <div class="card w-100 mb-6">
-            <div class="card-body text-center d-flex flex-column justify-content-between py-0">
-                <div class="my-5 flex-grow-1">
-                    <p class="fw-bolder text-gray-700 m-0 fs-3 text-center">{{ $sector->name }}</p>
-                    <div>
-                        @if ($sector->groups->count())
-                            <p class="fw-bolder text-gray-700 m-0 fs-6 text-center">Grupos associados</p>
-                            @foreach ($sector->groups as $group)
-                            <p class="text-gray-700 m-0 fs-7 text-center">{{ $group->name }}</p>
-                            @endforeach
-                        @else
-                            <span class="badge badge-light">
-                                Sem Grupos
-                            </span>
-                        @endif
-                    </div>
-                </div>
-                <div class="d-flex">
-                    <a href="{{ route('sectors.edit', $sector->id) }}" class="btn btn-sm btn-light-primary w-100">
-                        Acessar Módulo
-                    </a>
-                </div>
-                    @if ($sector->status == 0)
-                    <span class="btn btn-outline btn-outline-dashed btn-outline-danger px-4 py-1 disabled my-3 w-100">Desativado</span>
-                    @else
-                    <span class="btn btn-outline btn-outline-dashed btn-outline-success px-4 py-1 disabled my-3 w-100">Ativado</span>
-                    @endif
+            <div class="card-header d-flex align-items-center justify-content-between min-h-50px">
+                <p class="fw-bolder @if ($sector->status == 0) text-danger @else text-gray-700 @endif  m-0 fs-3 text-center text-uppercase">{{ $sector->name }}</p>
+                <a href="{{ route('sectors.edit', $sector->id) }}" class="btn btn-sm btn-icon btn-light-primary">
+                    A
+                </a>
             </div>
-            <div class="card-footer p-2">
+            <div class="card-body text-center">
+                @if ($sector->groups->count())
+                    @foreach ($sector->groups as $group)
+                    <span class="badge badge-light-primary mb-2">{{ $group->name }}</span>
+                    @endforeach
+                @else
+                    <p class="text-gray-500 text-center fs-7 fw-bold mb-0">
+                        Sem Grupos
+                    </p>
+                @endif
+            </div>
+            {{-- <div class="card-footer p-2 border-0">
                 <p class="text-gray-600 text-center m-0 fs-8">
                     <span class="fw-bold">Criado: </span>{{ $sector->created_at->format('d/m/Y') }} às {{ $sector->created_at->format('H:i') }}
                 </p>
-            </div>
+            </div> --}}
         </div>
     </div>
     @endforeach
