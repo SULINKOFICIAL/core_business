d5b4f7dfec3dbbbe3c68fa261d774539be98bc0a
JeandreoFurquim <jeandreofur@gmail.com>
2025-03-11 15:15:20 -0300

    Update

--- app/Http/Controllers/ClientsActionsController.php
+++ app/Http/Controllers/ClientsActionsController.php
@@ -81,7 +81,7 @@ class ClientsActionsController extends Controller
         }
 
         // Realiza a solicitação
-        $response = $guzzle->$method("http://$client->domain/api/$url", $options);
+        $response = $guzzle->$method("https://$client->domain/api/$url", $options);
 
         // Obtém o corpo da resposta
         $response = $response->getBody()->getContents();
