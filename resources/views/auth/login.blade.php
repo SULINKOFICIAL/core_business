07f31fe036e049188fb6b605d93547ea45785c27
JeandreoFurquim <jeandreofur@gmail.com>
2025-03-14 16:47:02 -0300

    Update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,37 +5,34 @@
 		<title>Login</title>
 		@include('layouts.head')
 	</head>
-	<body id="kt_body" class="app-blank bgi-size-cover bgi-attachment-fixed bgi-position-center">
+	<body id="kt_body" class="app-blank bgi-size-cover bgi-attachment-fixed bgi-position-center" style="background: url('{{ asset('assets/media/images/background.jpg') }}');background-position: left top;background-size: cover;">
         @include('layouts.config')
 		<div class="d-flex flex-column flex-root" id="kt_app_root">
 			<style>body { background-image: url('{{ asset("assets/media/auth/bg10.jpeg") }}'); } [data-bs-theme="dark"] body { background-image: url('{{ asset("assets/media/auth/bg10-dark.jpeg") }}'); }</style>
 			<div class="d-flex flex-column flex-lg-row flex-column-fluid">
 				<div class="d-flex flex-lg-row-fluid">
-					<div class="d-flex flex-column flex-center pb-0 pb-lg-10 p-10 w-100">
-                        <img class="theme-light-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="{{ asset('assets/media/logos/sulink-core-dark.svg') }}" alt="" />
-                        <h1 class="text-gray-800 fs-2qx fw-bold text-center mb-7">Sistema de Gerenciamento Core</h1>
-                        <div class="text-gray-600 fs-base text-center fw-semibold">
-                            Este sistema foi projetado para otimizar e centralizar as operações empresariais,
-                            <a href="#" class="opacity-75-hover text-primary me-1">facilitando a<br> gestão</a> e promovendo maior eficiência.
-                            Com funcionalidades integradas e automação de processos,<br /> o Core é a solução ideal para
-                            <a href="#" class="opacity-75-hover text-primary me-1">empresas que buscam evolução</a> na gestão de seus recursos.
-                        </div>
-                    </div>
-
 				</div>
-				<div class="d-flex flex-column-fluid flex-lg-row-auto justify-content-center justify-content-lg-end p-12">
-					<div class="bg-body d-flex flex-column flex-center rounded-4 w-md-600px p-10">
-						<div class="d-flex flex-center flex-column align-items-stretch h-lg-100 w-md-400px">
-							<div class="d-flex flex-center flex-column flex-column-fluid pb-15 pb-lg-20">
-								<form class="form w-100" method="POST" action="{{ route('login') }}">
-                                    @csrf
-                                    <h1 class="text-gray-900 fw-bolder text-center mb-3">Acessar</h1>
-                                    <input type="text" placeholder="Email" name="email" class="form-control bg-transparent mb-8" required/>
-                                    <input type="password" placeholder="Password" name="password" class="form-control bg-transparent mb-3" required />
-                                    <button type="submit" class="btn btn-primary">
-                                        Acessar
-                                    </button>
-								</form>
+				<div class="d-flex align-items-center pe-5">
+					<div class="text-center">
+						<img class="theme-light-show mx-auto mw-100 w-150px w-lg-400px mb-10 mb-lg-15" src="{{ asset('assets/media/logos/logo-central-dark.svg') }}" alt="" />
+						<div class="bg-body d-flex flex-column flex-center rounded-4 w-md-600px px-7 py-15">
+							<div class="d-flex flex-center flex-column align-items-stretch h-lg-100 px-10">
+								<div class="d-flex flex-center flex-column flex-column-fluid">
+									<form class="form w-100" method="POST" action="{{ route('login') }}">
+										@csrf
+										<h1 class="text-gray-800 fs-1 fw-bold text-center mb-4">Acessar Gerenciamento de miCore</h1>
+										<div class="text-gray-600 fs-base text-center fw-semibold mb-7">
+											Este sistema foi projetado para otimizar e centralizar as operações empresariais, facilitando a gestãoe promovendo maior eficiência.
+										</div>
+										<input type="text" placeholder="Email" name="email" class="form-control bg-transparent mb-8" required/>
+										<input type="password" placeholder="Password" name="password" class="form-control bg-transparent mb-3" required />
+										<div class="d-flex justify-content-end">
+											<button type="submit" class="btn text-uppercase btn-success btn-active-danger fw-bolder">
+												Acessar
+											</button>
+										</div>
+									</form>
+								</div>
 							</div>
 						</div>
 					</div>
