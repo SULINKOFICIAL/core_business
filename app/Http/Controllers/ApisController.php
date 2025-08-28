68ec9ef8d4258f0f7519b9ffee53f8fa8352b068
JeandreoFurquim <jeandreofur@gmail.com>
2025-08-28 15:19:36 -0300

    Atualização: Configurações - Separadas por Tenant

--- app/Http/Controllers/ApisController.php
+++ app/Http/Controllers/ApisController.php
@@ -173,6 +173,7 @@ class ApisController extends Controller
 
         // Retorna os dados do banco de dados
         return response()->json([
+            'tenant'        => $client->id,
             'database_name' => $client->table,
             'db_user'       => $client->table_user,
             'db_password'   => $client->table_password,
