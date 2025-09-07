6bc4f83e8782dc764dec8ee78cb047b8553a6312
JeandreoFurquim <jeandreofur@gmail.com>
2025-09-07 15:21:44 -0300

    Atualização: Sistema de notícias

--- resources/views/pages/news/show.blade.php
+++ resources/views/pages/news/show.blade.php
@@ -8,32 +8,27 @@
 </p>
 <div class="card w-1000px m-auto">
     <div class="card-header align-items-center">
-        <h2 class="m-0">Avisos</h2>
+        <h2 class="m-0">Aviso: <b class="text-primary">{{ $news->title }}</b></h2>
     </div>
     <div class="card-body news-body">
-        <h2>{{ $news->title }}</h2>
         <div>
             {!! $news->body !!}
         </div>
+        @if ($news->cta_text)
+            <button class="btn btn-primary btn-active-danger mt-4">
+                {{ $news->cta_text }}
+            </button>
+        @endif
     </div>
 </div>
-<div class="d-flex justify-content-between mt-4 w-1000px m-auto">
-    @if ($news->status == 1)
-        <a href="{{ route('news.destroy', $news->id) }}" class="btn btn-lg btn-danger me-2">
-            Desativar Notícia
+<div class="d-flex justify-content-end mt-4 w-1000px m-auto">
+    <div>
+        <a href="{{ route('news.index') }}" class="btn btn-light text-muted me-2">
+            Voltar
         </a>
-        @else
-        <a href="{{ route('news.destroy', $news->id) }}" class="btn btn-lg btn-success me-2">
-            Ativar Notícia
+        <a href="{{ route('news.edit', $news->id) }}" class="btn btn-primary btn-active-danger">
+            Editar
         </a>
-    @endif
-        <div>
-            <a href="{{ url()->previous() ?? route('news.index') }}" class="btn btn-light text-muted me-2">
-                Voltar
-            </a>
-            <a href="{{ route('news.edit', $news->id) }}" class="btn btn-primary btn-active-danger">
-                Editar
-            </a>
-        </div>
+    </div>
 </div>
 @endsection
