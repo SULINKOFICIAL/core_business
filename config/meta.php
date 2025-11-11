fd9be6ce38ec8069be06017096f54c6d16d40401
JeandreoFurquim <jeandreofur@gmail.com>
2025-11-11 20:03:35 -0300

    Merge remote-tracking branch 'origin'

---
+++ config/meta.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Configurações do Meta
+ * 
+ * Parametros obtidos no aplicativo:
+ * https://developers.facebook.com/apps/3920427851582223/
+ * 
+ */
+return [
+    'client_id' => '3920427851582223',
+    'client_secret' => 'ae00838edd8b3d5d7bcbb480e47b95a4',
+];
