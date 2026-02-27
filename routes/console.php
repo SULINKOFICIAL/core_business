46081b89795d1370385961b7a6239dfd2cda68c2
Jeandreo <jeandreo@sulink.com.br>
2026-02-27 11:41:28 -0300

    Merge remote-tracking branch 'origin'

--- routes/console.php
+++ routes/console.php
@@ -1,25 +1,7 @@
 <?php
 
-use App\Jobs\ChargeSubscriptions;
-use App\Jobs\GenerateRenewalOrders;
 use App\Jobs\ScheduleDispatcher;
 use Illuminate\Support\Facades\Schedule;
-use Illuminate\Support\Facades\Log;
-
-/**
- * Gera os pedidos de renovação dos clientes.
- */
-Schedule::job(GenerateRenewalOrders::class)
-    ->dailyAt('03:00')
-    ->withoutOverlapping();
-
-/**
- * Realiza cobrança dos pedidos dos clientes
- */
-Schedule::job(ChargeSubscriptions::class)
-    ->dailyAt('07:30')
-    ->withoutOverlapping();
-
 
 /**
  * Jobs agendados para serem executados em todos os servidores
