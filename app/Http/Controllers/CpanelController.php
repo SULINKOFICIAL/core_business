525f89850ff53c11dbf652017590d210b26afd7a
IsPrt <isispertsbr@gmail.com>
2026-03-26 11:28:21 -0300

    update

--- app/Http/Controllers/CpanelController.php
+++ app/Http/Controllers/CpanelController.php
@@ -291,7 +291,7 @@ class CpanelController extends Controller
         // Obtem o pacote do cliente
         $package = $client->package;
 
-        dd($package->modules()->toArray());
+        dd($package->items()->pluck('item_id')->toArray());
 
         // Inicia serviço de módulos
         $moduleService = app(ModuleService::class);
